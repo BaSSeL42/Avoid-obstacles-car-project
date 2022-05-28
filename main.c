ba1d86c2f1fbd9713e28c76e2b30ee7df09bb2d3
BaSSeL42 <104660108+BaSSeL42@users.noreply.github.com>
2022-05-28 15:46:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,12 +5,17 @@
 * Author : yasser
 */
 #define F_CPU		16000000UL
+
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "servo.h"
 #include "ultrasonic.h"
+#include "LCD.h"
 
-#include "timers_grp_25.h"
+//#include "timers_grp_25.h"
 #include "dcMotor.h"
 
 
@@ -18,37 +23,45 @@
 
 int main(void)
 {
-	
-	TIMER1_ICR_init();
+	LCD_INIT();
+	//TIMER1_ICR_init();
 	servoMotorInit();
+	extInt_init();
 	motorInit();
 	//Timer0_Init();
 	//Timer0_Start();
 	
-	Set_Bit(DDRC, 7);
-
 	f64 distR, distL;
+	
+	Set_Bit(DDRC, 7);
 	
 	while (1)
 	{
 
+
 		servoMotorRotate(3000);   // set servo at 0 degree
+		
 		motorStart();
 		
-		if (UltrasonicRead() < 50.0)
+		if (ultraRead() < 50.0)
 		{
 			motorStop();
-			
+			Set_Bit(PORTC, 7);
 			
 			servoMotorRotate(2000);
-			distL = ultrasonic_read();
-			_delay_ms(1000);
+			distL = ultraRead();
+			_delay_ms(250);
 			servoMotorRotate(5250);
-			distR = ultrasonic_read();
-			_delay_ms(1000);
+			distR = ultraRead();
+			_delay_ms(250);
 			
 			if (distL > distR)
 			{
+				LCD_Clear();
+				LCD_GoTo(1,1);
+				LCD_WriteSTRING("Turn Left");
+				LCD_GoTo(2,1);
+				LCD_WriteINTEGER((Uint32) distL);
 				backward();
 				_delay_ms(1000);
 				turnLeft();
@@ -57,6 +70,12 @@ int main(void)
 			}
 			else
 			{
+				LCD_Clear();
+				LCD_GoTo(1,1);
+				LCD_WriteSTRING("Turn Right");
+				LCD_GoTo(2,1);
+				LCD_WriteINTEGER((Uint32) distR);
+				
 				backward();
 				_delay_ms(1000);
 				turnRight();
@@ -64,7 +83,6 @@ int main(void)
 				//Set_CallBack(motorStop);
 			}
 		}
-
 	}
 }
 
