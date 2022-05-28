126034be8fe9bb5d507823cd7a41412f1930bcd9
BaSSeL42 <104660108+BaSSeL42@users.noreply.github.com>
2022-05-28 02:46:43 +0200

    Update ultrasonic.c

--- ultrasonic.c
+++ ultrasonic.c
@@ -19,25 +19,38 @@
 #define	TOIE1		2
 #define	TICIE1		5
 
+#define	TOV0		0
+#define	TOIE0		0
+#define  CS00		0
+#define  CS01		1
+#define INT0		6
+
+#define ISC00		0
+#define ISC01		1
+#define INTF0		6
+
 #define trig		0
 
 /*****  For input capture and ovf interrupt (ISR)   *****/
-volatile Uint8 ovf , ov;
+volatile Uint8 ovf , ov, ovff, overflow=0;
 volatile Uint16 cap1, cap2;
 volatile Uint8 mask = 0;
 /*******************************************/
 
 /* Some variables */
 Uint32 t_ticks;
+Uint32 ticks;
 f64 distance;   // double distance
 f64 dist;   // double distance
+f64 distt;   // double distance
 /*********************/
 
 #define  Trigger_pin	0	/* Trigger pin */
 volatile int TimerOverflow = 0;
 long count;
 
-volatile Uint8 flag = 1; // for external interrupt
+volatile Uint8 flag = 0; // for external interrupt
+volatile Uint16 cap11, cap12;
 
 
 /********************************  CODE ***************************/
@@ -105,31 +118,76 @@ f64 UltrasonicRead(void)
 
 void extInt_init(void)
 {
+	cli();
 	
 	Clear_Bit(DDRD, 2);  // set PD2 as input for external interrupt
 	//DDRC |= 0xFF;
 	Set_Bit(DDRA, trig); /* Make trigger pin as output */
 	
-	Set_Bit(DDRC, 7);
+	TCNT0 = 0;
+	TCCR0 = 0;		/* Set all bit to zero Normal operation */
+	
+	
+	Set_Bit(TCCR0, CS00); // set prescalar to 1
 	
 	/*  set external interrupt sense rising edge */
 	Set_Bit(MCUCR, ISC00);
 	Set_Bit(MCUCR, ISC01);
 	
-	//PORTD = 0xFF;		/* Turn on Pull-up */
+	Set_Bit(GICR, INT0); // enable interrupt 0
+	Set_Bit(TIMSK, TOIE0); /* Enable Timer0 overflow interrupts */
 	
 	sei();			/* Enable global interrupt */
-	Set_Bit(TIMSK, TOIE1); /* Enable Timer1 overflow interrupts */
-	Set_Bit(GICR, INT0); // enable interrupt 0
-	TCCR1A = 0;		/* Set all bit to zero Normal operation */
-	
-	Set_Bit(TCCR1B, CS11); // set prescalar to 8
-	ultrasonic_trig();
-	
-	
 }
 
 /************************************************************************************************************/
+
+
+f64 ultraRead(void)
+{
+	/* send high to trigger bin within 10 us turn it off or send one pulse at a time */
+	Clear_Bit(PORTA, trig);
+	_delay_us(2);
+	
+	Set_Bit(PORTA, trig);
+	_delay_us(10);
+	Clear_Bit(PORTA, trig);
+	/***************************************************/
+	
+	
+	if (flag == 2)
+	{
+		flag = 0;
+		ticks = cap12 + (256*ovff) ;
+		
+		// sound velocity = 34300 cm/sec
+		// tick time = 1/(F_CPU) as 1 is prescalar -> tick_time = 62.5 ns -> 62.5e-9 sec
+		// total_time = t_ticks * 62.5e-9 sec
+		// distance = (sound_velocity * total_time)/2 -> (343000 * total_time)/2 -> (17150 * t_ticks * 62.5e-9) -> (t_ticks / 932.95)
+		distt = (f64) ticks / 932.95;
+		
+		//strcat(string, "cm    ");
+		//dtostrf(distance,3,2, string); //  to print distance on LCD
+		
+		ovff=0; overflow=0;
+		
+		/*  set external interrupt sense rising edge */
+		Set_Bit(MCUCR, ISC00);
+		Set_Bit(MCUCR, ISC01);
+		
+		/* set input ovf flag to 1 to disable it */
+		Set_Bit(TIFR, TOV0);
+		Set_Bit(GIFR, INTF0);
+		/*************************/
+		
+		/* enable external interrupt and overflow interrupt */
+		Set_Bit(TIMSK, TOIE0);
+		Set_Bit(GICR, INT0);
+		/**************************/
+		
+	}
+	return distt;
+}
 
 
 
@@ -171,62 +229,61 @@ ISR(TIMER1_OVF_vect)
 }
 
 
-		
-		
-ISR(INT0_vect)
+ISR(TIMER0_OVF_vect)
 {
-	if (flag == 1)
-	{
-		Clear_Bit(MCUCR, ISC00);
-		Set_Bit(MCUCR, ISC01);
-		
-		TCNT1 = 0;
-		flag = 0;
-	}
-	else
-	{
-		Uint32 count;
-		f64 dist;
-		
-		count = TCNT1 + (65535 * ov);
-		
-		dist = (f64) count /  466.47;
-		
-		if (dist < 15.0)
-		{
-			Set_Bit(PORTC, 7);
-		}
-		else
-		{
-			Clear_Bit(PORTC, 7);
-		}
-		
-		ultrasonic_trig();
-		
-		flag = 1;
-	}
+	overflow++;
 }
 
+ISR(INT0_vect)
+{
+	if (flag == 0)
+	{
+		TCNT0 = 0 ;
+		Clear_Bit(MCUCR, ISC00);
+		Set_Bit(MCUCR, ISC01);
+		overflow = 0;
+	}
+	else if(flag == 1)
+	{
+		
+		cap12 = TCNT0;
+		ovff = overflow;
+		
+		Clear_Bit(TIMSK, TOIE0);
+		Clear_Bit(GICR, INT0);
+	}
+	flag++;
+}
+
+
+
+
+
+
+
+
+
+
 //cap1 = ICR1L | (ICR1H << 8);
- ISR(TIMER1_CAPT_vect)
- {
- 	if (mask == 0)
- 	{
- 		cap1 = ICR1;
- 		Clear_Bit(TCCR1B, ICES1);
- 		ov = 0;
- 	}
- 	else if (mask == 1)
- 	{
- 		
- 		cap2 = ICR1;
- 		ovf = ov;
+ISR(TIMER1_CAPT_vect)
+{
+	if (mask == 0)
+	{
+		cap1 = ICR1;
+		Clear_Bit(TCCR1B, ICES1);
+		ov = 0;
+	}
+	else if (mask == 1)
+	{
+		
+		cap2 = ICR1;
+		ovf = ov;
 
- 		/*disable any interrupt to prevent any nested interrupt*/
- 		Clear_Bit(TIMSK, TOIE1);
- 		Clear_Bit(TIMSK, TICIE1);
- 		/****************************/
+		/*disable any interrupt to prevent any nested interrupt*/
+		Clear_Bit(TIMSK, TOIE1);
+		Clear_Bit(TIMSK, TICIE1);
+		/****************************/
 
- 	}
- 	mask++;
- }
+	}
+	mask++;
+}
